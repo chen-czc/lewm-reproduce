82d99d120c196eb49f103f84b822975781f48025
quentinll <quentin.lelidec@protonmail.com>
2026-05-15 14:03:13 -0400

    using hydra to instantiate lewm

--- train.py
+++ train.py
@@ -10,8 +10,7 @@ import torch
 from lightning.pytorch.loggers import WandbLogger
 from omegaconf import OmegaConf, open_dict
 
-from jepa import JEPA
-from module import ARPredictor, Embedder, MLP, SIGReg
+from module import SIGReg
 from utils import get_column_normalizer, get_img_preprocessor, SaveCkptCallback
 
 
@@ -63,11 +62,10 @@ def run(cfg):
         for col in cfg.data.dataset.keys_to_load:
             if col.startswith("pixels"):
                 continue
-
             normalizer = get_column_normalizer(dataset, col, col)
             transforms.append(normalizer)
 
-            setattr(cfg.wm, f"{col}_dim", dataset.get_dim(col))
+        cfg.model.action_encoder.input_dim = cfg.data.dataset.frameskip * dataset.get_dim("action")
 
     transform = spt.data.transforms.Compose(*transforms)
     dataset.transform = transform
@@ -84,49 +82,7 @@ def run(cfg):
     ##       model / optim      ##
     ##############################
 
-    encoder = spt.backbone.utils.vit_hf(
-        cfg.encoder_scale,
-        patch_size=cfg.patch_size,
-        image_size=cfg.img_size,
-        pretrained=False,
-        use_mask_token=False,
-    )
-
-    hidden_dim = encoder.config.hidden_size
-    embed_dim = cfg.wm.get("embed_dim", hidden_dim)
-    effective_act_dim = cfg.data.dataset.frameskip * cfg.wm.action_dim
-
-    predictor = ARPredictor(
-        num_frames=cfg.wm.history_size,
-        input_dim=embed_dim,
-        hidden_dim=hidden_dim,
-        output_dim=hidden_dim,
-        **cfg.predictor,
-    )
-
-    action_encoder = Embedder(input_dim=effective_act_dim, emb_dim=embed_dim)
-    
-    projector = MLP(
-        input_dim=hidden_dim,
-        output_dim=embed_dim,
-        hidden_dim=2048,
-        norm_fn=torch.nn.BatchNorm1d,
-    )
-
-    predictor_proj = MLP(
-        input_dim=hidden_dim,
-        output_dim=embed_dim,
-        hidden_dim=2048,
-        norm_fn=torch.nn.BatchNorm1d,
-    )
-
-    world_model = JEPA(
-        encoder=encoder,
-        predictor=predictor,
-        action_encoder=action_encoder,
-        projector=projector,
-        pred_proj=predictor_proj,
-    )
+    world_model = hydra.utils.instantiate(cfg.model)
 
     optimizers = {
         'model_opt': {
@@ -162,7 +118,7 @@ def run(cfg):
         OmegaConf.save(cfg, f)
 
     object_dump_callback = SaveCkptCallback(
-        run_name=cfg.output_model_name, cfg=cfg, epoch_interval=1,
+        run_name=cfg.output_model_name, cfg=cfg.model, epoch_interval=1,
     )
 
     trainer = pl.Trainer(
